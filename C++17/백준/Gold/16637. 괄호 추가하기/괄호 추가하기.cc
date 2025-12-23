d13bba0bb6eb8d08d53398e41d4067ad687ca015
6uchu <74772618+6uchu@users.noreply.github.com>
2025-12-24 03:54:12 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char c[14]; //max 9개
+int n[14]; //max 10개
+int a, num, ret = -987654321;
+int cc, nn;
+
+int cal(int k, int front, int rear){
+    if(c[k] == '+') return front + rear;
+    if(c[k] == '-') return front - rear;
+    if(c[k] == '*') return front * rear;
+}
+
+void go(int i, int val){
+    if(i == nn - 1)
+        if(ret < val) ret = val;
+
+    if(i + 1 < nn) // <- 이거 필요한 이유?
+        go(i + 1, cal(i, val, n[i + 1]));
+    if(i + 2 < nn)
+        go(i + 2, cal(i, val, cal(i + 1, n[i + 1], n[i + 2])));
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> num;
+    string str; cin >> str;
+
+    for(int i = 0; i < num; i++){
+        if(i & 1) c[cc++] = str[i];
+        else n[nn++] = str[i] - '0';
+    }
+
+    go(0, n[0]);
+
+    cout << ret;
+}
